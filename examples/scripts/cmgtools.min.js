8eecac6bfa373c0b204b6e92567332af58420a96
Bhagwat Chouhan <bhagwat.chouhan@gmail.com>
2017-01-25 16:39:58 +0530

    Rebuilt for alpha4 release.

--- examples/scripts/cmgtools.min.js
+++ examples/scripts/cmgtools.min.js
@@ -1,5 +1,5 @@
 /**
- * CMGTools JS - v1.0.0-alpha1 - 2016-11-12
+ * CMGTools JS - v1.0.0-alpha1 - 2017-01-25
  * Description: CMGTools JS is a JavaScript library which provide utilities, ui components and MVC framework implementation for CMSGears.
  * License: GPLv3
  * Author: Bhagwat Singh Chouhan
